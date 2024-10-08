491438ab2f5f9e7139a6f8ec9677d5f9f7adf404
Janghana123 <quynhgiangknykn@gmail.com>
2024-10-08 15:22:08 +0700

    tao du an

---
+++ vaccin/src/test/java/com/spring/vaccin/VaccinApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.vaccin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VaccinApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
